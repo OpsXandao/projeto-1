69f574891b84ed8ca5860ae1a33fd78ab8887b95
Alexandre Paiva <109223675+OpsXandao@users.noreply.github.com>
2023-05-16 14:08:59 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'projeto-1';
+export class AppComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    OnDestroy,
+    AfterContentInit,
+    AfterViewInit,
+    AfterContentChecked,
+    AfterViewChecked
+{
+  title = 'Angular';
+
+  constructor() {
+    console.log('Component constructor');
+  }
+  ngAfterContentChecked(): void {
+    console.log('Component Content Check');
+  }
+  ngAfterViewChecked(): void {
+    console.log('After View Check');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('Content init');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Changes');
+  }
+
+  ngDoCheck(): void {
+    console.log('DoCheck');
+  }
+
+  ngOnDestroy(): void {
+    console.log('Destroyed');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('AfterViewInit');
+  }
+
+  ngOnInit(): void {
+    console.log('Component ngOnInit');
+  }
+
+  mudarTexto() {
+    this.title += 'A';
+  }
+
+  chamarFuncao() {
+    console.log("Esse é um click");
+  }
+
+  imgUrl =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/250px-Angular_full_color_logo.svg.png';
 }
